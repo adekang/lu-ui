ef44ca6dd62a2eda547d6145e053847da2cd8fbe
adekang <adekang@163.com>
2021-09-22 14:53:13 +0800

    实现 Icon  组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
-import qq from 'src/assets/qq.svg';
-
-console.log(qq);
+import Icon from 'src/components/Icon';
 
 function App() {
+  const ok = () => {
+    console.log('ok');
+  };
+
   return (
     <div>
-      <svg>
-        <use xlinkHref="#qq"/>
-      </svg>
+      <h1>你好</h1>
+      <Icon
+        name="qq"
+        color="red"
+        onClick={ok}
+        className="red"
+        onMouseEnter={() => console.log('enter')}
+        onMouseLeave={() => console.log('leave')}
+        onTouchStart={() => console.log('touch')}
+      />
     </div>
   );
 }
