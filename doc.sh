cdfe9bd33960b78bc19e7725f1793dbfdec71867
adekang <adekang@163.com>
2021-10-23 18:37:40 +0800

    updatee

--- doc.sh
+++ doc.sh
@@ -1,9 +1,10 @@
-#!/bin/env bash
-
-yarn build
-git checkout gh-pages
-build/* ./
-git add .
-git commit -m "update"
-git push
-git checkout master
+rm -rf build &&
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@github.com:adekang/lu-ui-web.git&&
+git push -f -u origin master &&
+cd -
